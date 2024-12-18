9ab0892e2a7cf59a279e5683160ee149fca77f31
Angel López Molina <alm.angel97@gmail.com>
2024-12-18 01:41:56 +0100

    Updated ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: angel <angel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 01:05:00 by angel             #+#    #+#             */
-/*   Updated: 2024/12/18 01:10:54 by angel            ###   ########.fr       */
+/*   Updated: 2024/12/18 01:33:33 by angel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,6 @@ size_t	ft_strlen(const char *str)
 		str++;
 	}
 	return (n);
-}
-
-char	*ft_strdup(const char *s)
-{
-	int		i;
-	char	*s_cpy;
-	char	len;
-
-	i = 0;
-	len = ft_strlen(s);
-	s_cpy = malloc(len + 1);
-	if (!s_cpy)
-		return (NULL);
-	while (s[i])
-	{
-		s_cpy[i] = s[i];
-		i++;
-	}
-	s_cpy[i] = 0;
-	return (s_cpy);
 }
 
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
