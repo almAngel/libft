0d770c2d5bc967c015c0812ff2f9429bb1622b87
Angel Lopez Molina <angellop@c1r1s4.42malaga.com>
2024-12-28 21:41:46 +0100

    Updated corrected & ready to evaluate

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,31 +6,30 @@
 /*   By: angellop <angellop@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 17:42:57 by angellop          #+#    #+#             */
-/*   Updated: 2024/12/28 19:33:01 by angellop         ###   ########.fr       */
+/*   Updated: 2024/12/28 21:40:44 by angellop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "malloc.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*response;
 	t_list	*parallel;
 
-	if (!lst || !f || !del)
+	if (!lst || !f)
 		return (NULL);
 	response = NULL;
 	while (lst)
 	{
 		parallel = ft_lstnew(f(lst->content));
 		if (!parallel)
-		{
-			ft_lstclear(&response, del);
-			del(parallel->content);
-			return (NULL);
-		}
-		ft_lstadd_back(&response, parallel);
+			ft_lstclear(&parallel, (*del));
+		else
+			ft_lstadd_back(&response, parallel);
 		lst = lst->next;
 	}
+	lst = NULL;
 	return (response);
 }
